9d3bf0a01512cfdeb29f0c7c1e67c05ce1b33b28
saqarmax-arm <62887825+saqarmax-arm@users.noreply.github.com>
2021-03-27 21:16:50 +0400

    Update updatescript.sh

--- updatescript.sh
+++ updatescript.sh
@@ -2,5 +2,5 @@
 remotehost=cronserver
 jsonpath=~/tmp/ccspeerinfo.json
 /usr/local/bin/ccs-cli getpeerinfo > ~/tmp/ccspeerinfo.json
-scp $jsonpath node@$remotehost:~/data/ccspeerinfo0.json
+scp $jsonpath node@$remotehost:~/data/ccspeerinfo.json
 rm $jsonpath
